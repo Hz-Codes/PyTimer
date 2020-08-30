1ecc9cf1ffeb5af33548312703ac05ce3fade31b
[Hamz]01001000 <62978695+Hamzah1010101@users.noreply.github.com>
2020-08-30 03:34:11 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,15 +3,27 @@
 cl=setterm
 
 ### START ###
+clear
+echo "Setup Started"
+sleep 1
 if [ -a $cl ];
 then 
+echo "Processing..."
+sleep 1
 cp alarm /bin
 cp alarm.sh /bin
 chmod 777 alarm.sh
 chmod 777 alarm
+echo "                   Completed ! 
+You Can Start By Typing 'alarm' On Your Terminal "
 else
+echo "Processing..."
+echo "Installing util-linux"
 sudo apt-get install util-linux
 cp alarm /bin
 cp alarm.sh /bin
 chmod 777 alarm.sh
 chmod 777 alarm
+echo "                   Completed ! 
+You Can Start By Typing 'alarm' On Your Terminal "
+fi
